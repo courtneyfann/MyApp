2dd2b048390d79c3df610bdd95e14bdd2f05f8ee
courtneyfann <cfann@ucsd.edu>
2025-10-27 23:41:44 -0700

    Updates

---
+++ _next/static/lQzdFZMOihMxDGzTVXyus/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/MyApp//_next/:path+",
+        "destination": "/MyApp/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
