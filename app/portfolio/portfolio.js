ec4684eeb12ed8ee248491da45eadbef61cfc768
courtneyfann <cfann@ucsd.edu>
2025-10-28 01:03:09 -0700

    edited homepage

--- app/portfolio/portfolio.js
+++ app/portfolio/portfolio.js
@@ -1,8 +1,14 @@
 export default function PortfolioPage() {
-  return (
-    <main>
-      <h1>Portfolio</h1>
-      <p>"This is my portfolio!"</p>
-    </main>
-  );
-}
+      return (
+        <div>
+          <h1>My Portfolio</h1>
+          <p>Welcome to my portfolio page! Here are some of my projects:</p>
+          
+          <ul>
+            <li>Project 1</li>
+            <li>Project 2</li>
+            <li>Project 3</li>
+          </ul>
+        </div>
+      );
+    }
