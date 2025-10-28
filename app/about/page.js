2253d6b72bf1e9f93b6d7e4ffc9dffd879714a80
courtneyfann <cfann@ucsd.edu>
2025-10-28 00:34:25 -0700

    added about and portfolio pages

---
+++ app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <main>
+      <h1>About</h1>
+      <p>This is the about page built with the Next.js App Router.</p>
+    </main>
+  );
+}
---
+++ app/portfolio/portfolio.js
@@ -0,0 +1,8 @@
+export default function PortfolioPage() {
+  return (
+    <main>
+      <h1>Portfolio</h1>
+      <p>"This is my portfolio!"</p>
+    </main>
+  );
+}
